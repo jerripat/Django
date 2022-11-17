a143b090dc2496ab27dbbb904f0c9436f4c3b9e0
jerripat <jerripatterson@gmail.com>
2022-11-16 21:48:04 -0600

    Update to program Enter file to update:

---
+++ lecture3/hello/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import  render
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello")
+
+def jerri(request):
+    return HttpResponse("Hello, Jerri")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
